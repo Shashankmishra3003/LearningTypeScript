3a9b6c33ac5b6a04557b057794e598c5cc128253
Shashankmishra3003 <Shashankmishra3003@gmail.com>
2020-05-14 11:04:25 -0700

    Types

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Code goes here!');
+function add(n1, n2, showresult, phrase) {
+    var Result = n1 + n2;
+    if (showresult) {
+        console.log(phrase + Result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var phrase = 'Result is: ';
+var result = add(number1, number2, printResult, phrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('Code goes here!');
+function add(n1:number, n2:number, showresult:boolean, phrase:string)
+{
+    const Result = n1 + n2;
+    if(showresult)
+    {
+        console.log(phrase + Result);
+    }
+    else
+    {
+        return n1 + n2;
+    }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const phrase = 'Result is: ';
+
+const result = add(number1,number2,printResult,phrase);
